b241c75ea73f93f26fea6f17a4b7e1e666bb755c
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-26 13:21:42 +0530

    variableAndDataType

---
+++ Chapter2-VariableAndDataTypes/PracticaSet/Problem3.py
@@ -0,0 +1,6 @@
+#Write a python program to find average of two numbers entered by the user
+
+a = int(input("Enter number first no. :- "))
+b = int(input("Enter number second no. :- "))
+
+print("The average of these two numbers is: ", (a+b)/2)

fc9611626dd731ba534b43989bf1ac70015baaa3
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-26 13:18:57 +0530

    variableAndDataType

---
+++ Chapter2-VariableAndDataTypes/4_operator.py
@@ -0,0 +1,28 @@
+# Arithmetic Operators
+a = 34
+b = 4
+c = a + b
+print(c)
+
+#Assignment Operators
+a = 4 -2 
+print(a)
+b = 6
+b +=3
+print(b)
+
+#Comparison Operators
+d = 12<8
+print(d)
+e = 12<=12
+print(e)
+
+#Logical Operators
+f = True or False
+print(f)
+g = True and False
+print(g)
+
+#Not operator
+print(not(True))
+print(not(False))

6db2f0a1d0487521aa82dd633d6facc92e3714d1
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-27 19:56:57 +0530

    Strings

---
+++ Chapter3-Strings/PracticeSet/Problem3.py
@@ -0,0 +1,5 @@
+#Write a program to detect double space in a string.
+
+name = "Rahul is good  boy and good  coder"
+
+print(name.find("  ")) #o/p-> 13 if you get -1 then it means it name does not contain double space

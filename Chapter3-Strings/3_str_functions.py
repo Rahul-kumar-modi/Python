86dcc91c4c0a5524956ee8d8958142d73c7735c2
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-27 19:52:02 +0530

    Strings

---
+++ Chapter3-Strings/3_str_functions.py
@@ -0,0 +1,9 @@
+name = "rohit"
+print(len(name)) #o/p-> 5
+print(name.endswith("it")) #o/p-> True
+print(name.startswith("Ro")) #o/p-> False
+print(name.capitalize()) #o/p-> Rohit
+
+s = "Harry is awesome coder"
+r = s.replace("awesome", "good")
+print(r) #o/p-> Harry is good coder

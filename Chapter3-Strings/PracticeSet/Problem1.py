c35ea2ab2572df729c9ce2407ea570c8f7db9878
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-27 19:55:49 +0530

    Strings

---
+++ Chapter3-Strings/PracticeSet/Problem1.py
@@ -0,0 +1,5 @@
+#Write a python program to display a user entered name followed by Good Afternoon using input() function. 
+
+name = input("Enter your name: ") #Printing "name" in Good Afternoon using fstring
+
+print(f"Good Afternoon {name}") 

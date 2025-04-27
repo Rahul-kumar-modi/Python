8db6a0870fc8276427ee0764f82ec7728a53c26e
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-27 19:54:05 +0530

    Strings

---
+++ Chapter3-Strings/4_escape_seq.py
@@ -0,0 +1,7 @@
+a = "Rahul is learning code \nto be good coder in industry" #For without using"""(triple quote) to print multiple line using "\n"
+
+print(a)
+
+b = "Rahul is learning code \nto be good coder in any \"industry\"" #For printing with double quote same for single quote to avoid confusion for complier which is ending quote.
+print(b) 
+

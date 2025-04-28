646899dbbad5dca888a4c3c0c18262487e6a5216
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-28 18:40:10 +0530

    ListsAndTuples

---
+++ Chapter4-ListAndTuples/2_listmethods.py
@@ -0,0 +1,15 @@
+friends= ["Carrot","Banana",3,False,"Suresh", 45.24,"Ramesh"]
+print(friends)  #o/p-> ['Carrot', 'Banana', 3, False, 'Suresh', 45.24, 'Ramesh']
+
+friends.append("Ladyfinger") 
+print(friends)  #o/p->['Carrot', 'Banana', 3, False, 'Suresh', 45.24, 'Ramesh', 'Ladyfinger]
+
+li = [13,11,43,9,7]
+# li.sort() #o/p-> [7, 9, 11, 13, 43]
+# li.reverse() #o/p-> [7, 9, 43, 11, 13]
+# li.insert(4,123);   #o/p->  [13, 11, 43, 9, 123, 7]
+li.pop(3)  #o/p-> [13, 11, 43, 7]
+print(li)
+
+
+

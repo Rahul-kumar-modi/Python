6fcea5ae2f5ff3fb556fbd83eef2bac02dc7c3c2
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-28 18:41:43 +0530

    ListsAndTuples

---
+++ Chapter4-ListAndTuples/PracticeSet/Problem1.py
@@ -0,0 +1,17 @@
+#Write a program to accept marks of 5 students and display them in sorted manner.
+marks = []
+
+f1 = int(input("Enter Marks here: "))
+marks.append(f1)
+f2 = int(input("Enter Marks here: "))
+marks.append(f2)
+f3 = int(input("Enter Marks here: "))
+marks.append(f3)
+f4 = int(input("Enter Marks here: "))
+marks.append(f4)
+f5 = int(input("Enter Marks here: "))
+marks.append(f5)
+
+marks.sort()
+
+print(marks)

c96351541f96f9fe6fe2ddf74b031c7e1d7bc70b
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-28 18:39:05 +0530

    ListsAndTuples

---
+++ Chapter4-ListAndTuples/1_list.py
@@ -0,0 +1,7 @@
+friends= ["Carrot","Banana",3,False,"Suresh", 45.24,"Ramesh"]
+
+print(friends[0])  #o/p-> Carrot
+friends[0] = "Radish" #Unlike Strings lists are mutable
+
+print(friends[0])  #o/p-> Radish
+print(friends[1:4])  #o/p-> ['Banana', 3, False]

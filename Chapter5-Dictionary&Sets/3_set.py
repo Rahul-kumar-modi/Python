ed24af89ec288f8c2db88636f97c0f624363bcd7
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-30 22:02:47 +0530

    Dictionary&Sets

---
+++ Chapter5-Dictionary&Sets/3_set.py
@@ -0,0 +1,5 @@
+s = {1, 5, 32, 34,5,2,5}  # Don't use s={} in for empty set as it will create an empty dictionary
+
+e = set() # use this for empty set
+
+print(s) #o/p-> {32, 1, 34, 2, 5} print distict element

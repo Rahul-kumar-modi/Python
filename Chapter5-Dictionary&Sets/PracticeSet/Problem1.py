6940b62d9bcdaf5e3f08ddcdcbb395fadc137a45
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-30 22:04:42 +0530

    Dictionary&Sets

---
+++ Chapter5-Dictionary&Sets/PracticeSet/Problem1.py
@@ -0,0 +1,12 @@
+#Write a program to create a dictionary of Hindi words with values as their English translation.Provide user with an option to look it up.
+
+words = {
+    "madad": "Help",
+    "nayak": "Hero",
+    "maafi": "Sorry"
+}
+
+word = input("Enter the word you want meaning of: ") #o/p->Enter the word you want meaning of: madad
+
+print(words[word])   #o/p->Help
+

9fd2cddbed847319c61f346a3db8c04cce700304
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-30 22:01:41 +0530

    Dictionary&Sets

---
+++ Chapter5-Dictionary&Sets/1_dict.py
@@ -0,0 +1,9 @@
+d = {} # Empty dictionary
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+print(marks, type(marks))  #o/p-> {'Harry': 100, 'Shubham': 56, 'Rohan': 23} <class 'dict'>
+print(marks["Harry"]) #o/p-> 100

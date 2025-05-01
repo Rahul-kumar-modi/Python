253b187a39317b563d7fba9753efe6f387779e7e
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-01 21:51:23 +0530

    ConditionalExpressions

---
+++ Chapter6-ConditionalExpressions/PracticeSet/Problem3.py
@@ -0,0 +1,11 @@
+# Write a program to find whether the name is in the list or not. 
+
+l = ["Rahul", "Rohit", "Shubham", "Divya"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")

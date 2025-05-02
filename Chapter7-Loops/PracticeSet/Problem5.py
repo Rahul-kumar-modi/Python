b3b11cb831b15f0c424918a9f7b91f38909b33a5
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-03 00:26:56 +0530

    loops

---
+++ Chapter7-Loops/PracticeSet/Problem5.py
@@ -0,0 +1,13 @@
+#Write a program to print * pattern like below and number is taken from user
+
+'''
+*
+**
+*** (n ==3)
+'''
+
+n = int(input("Enter a number: "))
+
+for i in range (1, n+1):
+  print("*"* i, end="")
+  print("")

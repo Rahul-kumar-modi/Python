20561665a0caf6afd5e608b18edbf9a2f4f0db81
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-03 00:23:19 +0530

    loops

---
+++ Chapter7-Loops/3_list_using_loop.py
@@ -0,0 +1,26 @@
+## For Loop with lists
+
+l = [1, "Rupesh", False, "This", "Rohan", "Shubham", "Mohan"]
+
+# i = 0
+
+# while(i<len(l)):
+#     print(l[i])
+#     i +=1
+
+for i in range(len(l)):
+    print(l[i])
+
+for i in l:
+    print(i)
+
+## For loop with Tuples
+t = (6, 231,45,21)
+for i in t:
+    print(i)
+
+## For loop with Strings
+s = "Rupesh"
+for i in s:
+    print(i)
+

1b046516dfee5e8fd5247e0e7778ff08a6058c7d
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-03 19:13:12 +0530

    Functions&Recursions

---
+++ Chapter8-Functions&Recursions/1_functions.py
@@ -0,0 +1,13 @@
+# Function Definition
+def avg():
+    a = int(input("Enter your 1st no. : "))
+    b = int(input("Enter your 2nd no. : "))
+    c = int(input("Enter your 3rd no. : "))
+
+    average = (a + b +c)/3
+    print(average)
+
+
+avg() #Function Call
+avg()
+avg()

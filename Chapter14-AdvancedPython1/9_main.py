9e284df36fae15ba516d7e4dc54a9e0cd9bb532a
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-11 23:51:27 +0530

    AdvancedPython

---
+++ Chapter14-AdvancedPython1/9_main.py
@@ -0,0 +1,6 @@
+from module import myFunc
+
+# We import myFunc function from module.py this make that we can access all the code from module.py in this file. But, 1 thing is different in line5 of module.py, the
+#  o/p-> This will print "module" because we import from module file.
+
+#if statement of line 7 of module.py will not run because in this __name__ = module not __main__.

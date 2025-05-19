8aebafeb411484683d9d4c00dda9c277a6e97145
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-05-19 22:04:21 +0530

    musicfile

---
+++ Chapter16-Project-Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth": "https://youtu.be/U47Tr9BB_wE?si=X8B1gvNBHrMJP8ap",
+    "jogi": "https://youtu.be/HexFqifusOk?si=CzramHDEd0EiNjIk",
+    "skyfall": "https://youtu.be/DeumyOzKqgI?si=c2uf-eVBDJMqRaOt",
+    "aarambh": "https://youtu.be/r6SbfF9FjTg?si=5JH0UpaXr9HKapT5",
+    "nazm": "https://youtu.be/DK_UsATwoxI?si=P8vO43bFN-B3EeB6"
+}
